b4571735468ef8009c8a0a25f5cb123f173387c0
DmytroKuts <40741910+Dmytro24@users.noreply.github.com>
2023-07-12 15:20:19 +0300

    fixing and optimizing scss styles

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Background from '../assets/background.jpg';
+import BackgroundMobile from '../assets/background-mobile.jpg';
 
 export const MainPage = () => {
+  const isMobile = window.innerWidth <= 768; 
+
   return (
     <section className="page__test">
       <div className="test__container">
         <div className="test__body body-test">
           <h1 className="body-test__title title">Test assignment for front-end developer</h1>
-          <div className="body-test__text text">
+          <div className="body-test__text">
             <p>
               What defines a good front-end developer is one that has skilled knowledge of HTML,
               CSS, JS with a vast understanding of User design thinking as they'll be building web
@@ -17,10 +20,11 @@ export const MainPage = () => {
               world of Front-End Development keeps evolving.
             </p>
           </div>
-          <Link to='/register' className="body-test__button button">Sign Up</Link>
+          <Link to="/register" className="body-test__button button">Sign Up</Link>
         </div>
-        <img src={Background} alt="Background" />
+        <img src={isMobile ? BackgroundMobile : Background} alt="Background" />
       </div>
     </section>
   );
 };
+
